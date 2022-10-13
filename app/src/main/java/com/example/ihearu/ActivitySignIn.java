bda6de3de82fc070f97c1681aa3225a8603d6089
Adi <adityadora277@gmail.com>
2022-10-13 15:00:26 -0400

    Initial home and login screen initialized

---
+++ app/src/main/java/com/example/ihearu/ActivitySignIn.java
@@ -0,0 +1,108 @@
+package com.example.ihearu;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ActivitySignIn extends AppCompatActivity {
+    GoogleSignInClient mclient;
+    ActivityResultLauncher<Intent> signInLauncher;
+    Bitmap pfpBit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        signInLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+                            handleSignInResult(task);
+
+                        }
+                    }
+                });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+         mclient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account != null){
+            try {
+                Log.d("PFP", String.valueOf(account.getPhotoUrl()));
+                pfpBit = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Objects.requireNonNull(account.getPhotoUrl()).normalizeScheme());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.putExtra("name", account.getDisplayName());
+            intent.putExtra("pfp", pfpBit);
+            Toast.makeText(this, "Signed in Successfully!", Toast.LENGTH_SHORT).show();
+            Log.d("Account", account.getEmail());
+            startActivity(intent);
+        }
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.sign_in_button:
+                        signIn();
+                        break;
+                }
+            }
+        });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mclient.getSignInIntent();
+        signInLauncher.launch(signInIntent);
+
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            Log.d("Account", account.toString());
+            Toast.makeText(this, "Signed in Successfully!", Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+}
--- app/src/main/java/com/example/ihearu/MainActivity.java
+++ app/src/main/java/com/example/ihearu/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.ihearu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.google.android.material.slider.Slider;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    Slider slider1, slider2, slider3;
+    Button confirmBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        slider1 = findViewById(R.id.slider1);
+
+        slider2 = findViewById(R.id.slider2);
+
+        slider3 = findViewById(R.id.slider3);
+
+        confirmBtn = findViewById(R.id.confirmBtn);
+
+
+
+
     }
 }
