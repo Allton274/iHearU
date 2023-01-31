f19819d7ad8b8b24b338be6d7ecf8acdd9373e11
Adi <adityadora277@gmail.com>
2023-01-30 22:22:46 -0500

    Latest update

--- app/src/main/java/com/example/ihearu/MainActivity.java
+++ app/src/main/java/com/example/ihearu/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -57,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         slider2 = findViewById(R.id.slider2);
 
         slider3 = findViewById(R.id.slider3);
-
 
 
 
